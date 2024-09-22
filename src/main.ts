b19c2fa4dfbda0891d16172fb10ea76d0e45d2d9
GnoxNahte <gnoxnahte.dev@gmail.com>
2024-09-22 12:47:20 +0800

    Removed, rename and cleanup code.
    Also created a src folder and shift main plugin code there

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { Plugin } from 'obsidian';
+import { DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab } from './settings-tab';
+
+export default class TabPanelsPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
---
+++ src/settings-tab.ts
@@ -0,0 +1,25 @@
+import { App, PluginSettingTab } from "obsidian";
+import TabPanelsPlugin from "./main";
+
+export interface MyPluginSettings {
+	mySetting: string;
+}
+
+export const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default'
+}
+
+export class SampleSettingTab extends PluginSettingTab {
+	plugin: TabPanelsPlugin;
+
+	constructor(app: App, plugin: TabPanelsPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+	}
+}
