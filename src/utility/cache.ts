accb762ff422757ee8841e087d9edf6326fdbb84
GnoxNahte <gnoxnahte.dev@gmail.com>
2024-12-06 16:13:43 +0800

    Add support for headings and tags. Also clean up some code

--- src/utility/cache.ts
+++ src/utility/cache.ts
@@ -1,29 +1,39 @@
 /**
- * Credits: This code is adapted from the HTML Tabs plugin.
+ * Credits: This base for the code is adapted from the HTML Tabs plugin.
  * Original source: https://github.com/ptournet/obsidian-html-tabs
  * Reference files:
  * - src/util/cache.ts
  * - src/util/parsing.ts
  */
 
-import { App, CachedMetadata, EmbedCache, FootnoteCache, HeadingCache, LinkCache, Loc, MetadataCache, ReferenceCache, TAbstractFile, TagCache, TFile } from "obsidian";
+import { App, CachedMetadata, CacheItem, EmbedCache, FootnoteCache, HeadingCache, LinkCache, Loc, MetadataCache, ReferenceCache, TAbstractFile, TagCache, TFile } from "obsidian";
 import TabPanelsPlugin from "src/main";
 
 export type DataCache = Record<string, CacheData>;
 
 interface TabsCache {
+    // Helps identify which cache is from this plugin, 
+    // In case Obsidian returns the array with the previous items in the cache.
     isFromTabPanels: boolean;
 }
 
-export interface TabsLinkCache extends LinkCache, TabsCache {}
-export interface TabsEmbedCache extends EmbedCache, TabsCache {}
+interface TabsLinkCache extends LinkCache, TabsCache {}
+interface TabsEmbedCache extends EmbedCache, TabsCache {}
+interface TabsHeadingCache extends HeadingCache, TabsCache {}
+interface TabsTagCache extends TagCache, TabsCache {}
+
+// To add a new item to cache:
+// 1. Create new function to parse item (Reference parseTagsByLine)
+// 2. Call it in rebuildCacheMetadata()
+// 3. Add updated cache to Obsidian cache, also in rebuildCacheMetadata()
+// 4. Add cache from settings to Obsidian cache (called when plugin loads) in updateCacheFromSettings()
 
 export interface CacheData {
     // Copied from Obsidian's CachedData
     links?: LinkCache[];
     embeds?: EmbedCache[];
-    tags?: TagCache[];
     headings?: HeadingCache[];
+    tags?: TagCache[];
     footnotes?: FootnoteCache[];
 
     // === NOT USING ===
@@ -45,26 +55,39 @@ export async function updateCacheFromSettings(settingsData: DataCache, metadataC
             continue;
         }
         
-        // Add the cached links in settings to Obsidian's cache
+        // ===== Add the cached data in settings to Obsidian's cache =====
+        // Add links
         const linkCache = settingsData[path].links;
         if (linkCache) {
             if (cachedMetadata.links)
                 cachedMetadata.links.push(...linkCache);
             else
                 cachedMetadata.links = linkCache;
-
-            // rebuildResolvedLinks(cachedMetadata, 
-            //     metadataCache, 
-            //     metadataCache.resolvedLinks[path],
-            //     metadataCache.unresolvedLinks[path]);
         }
 
+        // Add embeds
         const embedCache = settingsData[path].embeds;
         if (embedCache) {
             if (cachedMetadata.embeds)
                 cachedMetadata.embeds.push(...embedCache);
             else 
                 cachedMetadata.embeds = embedCache;
+        }
+
+        if (linkCache || embedCache) 
+            rebuildResolvedLinks(cachedMetadata, metadataCache, path);
+
+        // Add headings
+        const headingsCache = settingsData[path].headings;
+        if (headingsCache) {
+            if (cachedMetadata.headings) {
+                cachedMetadata.headings.push(...headingsCache);
+                // Not sure why need to sort when other metadata don't need to sort
+                // Order doesn't appear correctly in sidebar if never sort
+                cachedMetadata.headings.sort(sortCacheByOffset);
+            }
+            else 
+                cachedMetadata.headings = headingsCache;
         }
 
         // TODO: Add headings, embeds, etc to Obsidian's cache
@@ -101,14 +124,24 @@ export async function updateCacheOnFileDelete(plugin: TabPanelsPlugin, file: TAb
     if (settingsData[file.path])
     {
         delete settingsData[file.path];
-        plugin.saveSettings();
+        plugin.saveSettings(); 
     }
 }
 
 // Go through the whole vault and rebuild the whole cache in the settings.
 // TODO
 export async function rebuildVaultCache(settingsData: DataCache, plugin: TabPanelsPlugin) {
-    settingsData = {};
+    plugin.settings.dataCache = {};
+    const files = plugin.app.vault.getMarkdownFiles();
+    for (const file of files) {
+        const markdown = await plugin.app.vault.cachedRead(file);
+        const cachedMetadata = plugin.app.metadataCache.getFileCache(file);
+        if (!cachedMetadata) {
+            console.error("Error gettings cache for file: " + file.path);
+            continue;
+        }
+        updateCacheFromFile(plugin, file, markdown, cachedMetadata);
+    }
     
     await plugin.saveSettings();
 }
@@ -116,23 +149,25 @@ export async function rebuildVaultCache(settingsData: DataCache, plugin: TabPane
 // Parses the markdown, update the Obsidian's metadataCache and saves the result in settings
 export async function updateCacheFromFile(plugin: TabPanelsPlugin, file: TFile, markdown: string, cachedMetadata: CachedMetadata) {
     const metadataCache = plugin.app.metadataCache;
-
-    // Sometimes it'll return the previous cache. So to prevent adding new data to the previous data, filter out all the data from this plugin
+    
+    // Sometimes it'll pass in the previous cache. So to prevent adding new data to the previous data, filter out all the data from this plugin
     if (cachedMetadata.links) {
         cachedMetadata.links = cachedMetadata.links.filter((value) => !(value as TabsLinkCache).isFromTabPanels)
     }
-
+    if (cachedMetadata.embeds) {
+        cachedMetadata.embeds = cachedMetadata.embeds.filter((value) => !(value as TabsEmbedCache).isFromTabPanels)
+    }
+    if (cachedMetadata.headings) {
+        cachedMetadata.headings = cachedMetadata.headings.filter((value) => !(value as TabsHeadingCache).isFromTabPanels)
+    }
+    if (cachedMetadata.tags) {
+        cachedMetadata.tags = cachedMetadata.tags.filter((value) => !(value as TabsTagCache).isFromTabPanels)
+    }
+    // console.log("Raw headings", JSON.stringify(cachedMetadata, null, 2))
     const settingsCacheData = plugin.settings.dataCache;
     
-    // ===== Reset =====
-    // cachedMetadata.links = [];
+    // Reset 
     settingsCacheData[file.path] = {};
-    // if (settingsCacheData[file.path]) {
-    //     settingsCacheData[file.path].links = [];
-    // }
-    // Links
-    // metadataCache.resolvedLinks[file.path] = {};
-    // metadataCache.unresolvedLinks[file.path] = {};
 
     // Regex to get the markdown content
     // Note that there can be 3 or more backticks and the start and end backticks need to match
@@ -181,10 +216,15 @@ export async function updateCacheFromFile(plugin: TabPanelsPlugin, file: TFile, 
 
         // Get second capture group and update the cache
         const codeblockMarkdown = match[2];
-        const hasItemsInCache = updateCache(metadataCache, cachedMetadata, file.path, codeblockMarkdown, locOffset, settingsCacheData[file.path]);
-        if (hasItemsInCache)
+        const hasItemsInCache = rebuildCacheMetadata(codeblockMarkdown, locOffset, settingsCacheData[file.path]);
+        if (hasItemsInCache) {
+            rebuildResolvedLinks(cachedMetadata, metadataCache, file.path)
             hasItemsToCache = true;
+        }
     }
+
+    if (hasItemsToCache)
+        addToMetadataCache(cachedMetadata, settingsCacheData[file.path]);
 
     // If there's no items in the file, delete it from the cache
     if (!hasItemsToCache && settingsCacheData[file.path]) {
@@ -194,68 +234,29 @@ export async function updateCacheFromFile(plugin: TabPanelsPlugin, file: TFile, 
     await plugin.saveSettings();
 }
 
-// Returns true if it has items to put in the cache
-export function updateCache(metadataCache: MetadataCache, cachedMetadata: CachedMetadata, path: string, markdown: string, locOffset: Loc, outSettingsCacheData: CacheData): boolean {
-    
-    const hasItemsToCache = rebuildCacheMetadata(cachedMetadata, markdown, locOffset, outSettingsCacheData);
-    
-    // TODO: Not sure if need to update??
-    // if (hasItemsToCache){
-    //     rebuildResolvedLinks(cachedMetadata, metadataCache,
-    //                     metadataCache.resolvedLinks[path],
-    //                     metadataCache.unresolvedLinks[path]);
-    // }
-
-    return hasItemsToCache;
-}
-
 //#region Rebuilding CacheMetadata (From metadataCache.getFileCache)
 // Rebuilds the cache data, modifying outSettingsCacheData
 // Returns true if it has items to put in the cache
-function rebuildCacheMetadata(cachedMetadata: CachedMetadata, markdown: string, locOffset: Loc, outSettingsCacheData: CacheData): boolean {
-    parseLinks(markdown, locOffset, outSettingsCacheData);
-
-    // If it didn't change anything,
-    if (!outSettingsCacheData || Object.keys(outSettingsCacheData).length === 0)
-        return false;
-
-    // ===== Add result to Obsidian's cache =====
-    // TODO: Some code is repetitive, find a way to simplify it. Maybe add function with templates
-    // Add links
-    if (outSettingsCacheData.links && outSettingsCacheData.links.length > 0) {
-        const tabsLinkCache: TabsLinkCache[] = outSettingsCacheData.links.map((cache) => ({ ...cache, isFromTabPanels: true }));
-        if (!cachedMetadata.links){
-            cachedMetadata.links = tabsLinkCache;
-        }
-        else {
-            cachedMetadata.links?.push(...tabsLinkCache);
-        }
-    }
-
-    // Add embeds
-    if (outSettingsCacheData.embeds && outSettingsCacheData.embeds.length > 0) {
-        const tabsEmbedCache: TabsEmbedCache[] = outSettingsCacheData.embeds.map((cache) => ({ ...cache, isFromTabPanels: true }));
-        if (!cachedMetadata.embeds){
-            cachedMetadata.embeds = tabsEmbedCache;
-        }
-        else {
-            cachedMetadata.embeds?.push(...tabsEmbedCache);
-        }
-    }
-
-    return true;
-}
-
-function parseLinks(markdown: string, locOffset: Loc, outCacheData: CacheData) {
+function rebuildCacheMetadata(markdown: string, locOffset: Loc, outSettingsCacheData: CacheData): boolean {
     const lines = markdown.split("\n");
     let offset = 0;
     lines.forEach((line, lineNumber) => {
-        parseLinksByLine(line, locOffset.line + lineNumber, locOffset.offset + offset, outCacheData);
+        const currLineNum = locOffset.line + lineNumber;
+        const currOffset = locOffset.offset + offset;
+        parseLinksAndEmbedsByLine(line, currLineNum, currOffset, outSettingsCacheData);
+        parseHeadingsByLine(line, currLineNum, currOffset, outSettingsCacheData);
+        parseTagsByLine(line, lineNumber, currOffset, outSettingsCacheData);
         offset += line.length + 1;
     })
+
+    // If there's anything in outSettingsCacheData
+    return outSettingsCacheData && Object.keys(outSettingsCacheData).length > 0;
 }
 
-function parseLinksByLine(markdown: string, lineNumber: number, offset: number, outCacheData: CacheData)  {
+// Syntax for Links: [[Link]], [[Link|Display name]]
+// Syntax for Embeds: ![[Embed]], [[Embed|Display name]]
+// Links and embeds are similar. Only difference is embeds start with !
+function parseLinksAndEmbedsByLine(markdown: string, lineNumber: number, offset: number, outCacheData: CacheData)  {
     const linkRegex = /!?\[\[([^|\]]+)(?:\|([^\]]+))?\]\]/g;
 	const matches: RegExpMatchArray[] = [...markdown.matchAll(linkRegex)];
 	if (matches.length > 0) {
@@ -304,38 +305,178 @@ function parseLinksByLine(markdown: string, lineNumber: number, offset: number, 
             outCacheData.embeds = embeds;
 	}
 }
+
+// Syntax for headings: # Heading, ## Heading 2
+function parseHeadingsByLine(markdown: string, lineNumber: number, offset: number, outCacheData: CacheData) {
+    // Regex101 tests: https://regex101.com/r/Tydzgd/1
+    const headingRegex = /^[^\S\r\n]*(#{1,6}) +(.*)/m;
+    // match[0]: Full string
+    // match[1]: Hashtags to count level
+    // match[2]: Heading text
+    const match = markdown.match(headingRegex);
+	if (match) {
+        const col = match.index ? match.index : 0;
+        const start: Loc = {
+            line: lineNumber,
+            col: col,
+            offset: offset + col,
+        };
+        console.log("Result: ", match)
+        
+        const cache: HeadingCache = {
+            // Shouldn't have any space at the start, just trim end
+            heading: match[2].trimEnd(),
+            level: match[1].length,
+            position: {
+                start: start,
+                end: {
+                    line: start.line,
+                    col: start.col + match[0].length,
+                    offset: start.offset + match[0].length,
+                },
+            },
+        };
+
+        if (outCacheData.headings)
+            outCacheData.headings.push(cache);
+        else
+        outCacheData.headings = [cache];
+	}
+}
+
+// Syntax for tags: #tag
+function parseTagsByLine(markdown: string, lineNumber: number, offset: number, outCacheData: CacheData)  {
+    const tagRegex = /#[^\s~!@#$%^&*()+={}|:;"'<>?`[\]\\,.]+/g;
+	const matches: RegExpMatchArray[] = [...markdown.matchAll(tagRegex)];
+	if (matches.length > 0) {
+        const tags: TagCache[] = [];
+		matches.forEach((match: RegExpMatchArray) => {
+			const col = match.index ? match.index : 0;
+			const start: Loc = {
+				line: lineNumber,
+				col: col,
+				offset: offset + col,
+			};
+            
+            const cache: TagCache = {
+                tag: match[0],
+                position: {
+                    start: start,
+                    end: {
+                        line: start.line,
+                        col: start.col + match[0].length,
+                        offset: start.offset + match[0].length,
+                    },
+                },
+            };
+            tags.push(cache);
+		});
+        
+        if (outCacheData.tags) 
+            outCacheData.tags.push(...tags);
+        else
+            outCacheData.tags = tags;
+	}
+}
+
+function addToMetadataCache(cachedMetadata: CachedMetadata, outSettingsCacheData: CacheData) {
+    // ===== Add result to Obsidian's cache =====
+    // TODO: Some code is repetitive, find a way to simplify it. Maybe add function with templates
+    // Add links
+    if (outSettingsCacheData.links && outSettingsCacheData.links.length > 0) {
+        const tabsLinkCache: TabsLinkCache[] = outSettingsCacheData.links.map((cache) => ({ ...cache, isFromTabPanels: true }));
+        if (!cachedMetadata.links){
+            cachedMetadata.links = tabsLinkCache;
+        }
+        else {
+            cachedMetadata.links?.push(...tabsLinkCache);
+        }
+    }
+
+    // Add embeds
+    if (outSettingsCacheData.embeds && outSettingsCacheData.embeds.length > 0) {
+        const tabsEmbedCache: TabsEmbedCache[] = outSettingsCacheData.embeds.map((cache) => ({ ...cache, isFromTabPanels: true }));
+        if (!cachedMetadata.embeds){
+            cachedMetadata.embeds = tabsEmbedCache;
+        }
+        else {
+            cachedMetadata.embeds?.push(...tabsEmbedCache);
+        }
+    }
+
+    // Add headings
+    if (outSettingsCacheData.headings && outSettingsCacheData.headings.length > 0) {
+        const tabsHeadingCache: TabsHeadingCache[] = outSettingsCacheData.headings.map((cache) => ({ ...cache, isFromTabPanels: true }));
+        if (!cachedMetadata.headings){
+            cachedMetadata.headings = tabsHeadingCache;
+        }
+        else {
+            cachedMetadata.headings?.push(...tabsHeadingCache);
+        }
+
+        // Not sure why need to sort when links don't need to sort
+        // Order doesn't appear correctly in sidebar if never sort
+        cachedMetadata.headings.sort(sortCacheByOffset)
+    }
+
+    if (outSettingsCacheData.tags && outSettingsCacheData.tags.length > 0) {
+        const tabsTagsCache: TabsTagCache[] = outSettingsCacheData.tags.map((cache) => ({ ...cache, isFromTabPanels: true }));
+        if (!cachedMetadata.tags){
+            cachedMetadata.tags = tabsTagsCache;
+        }
+        else {
+            cachedMetadata.tags?.push(...tabsTagsCache);
+        }
+    }
+
+}
+
 //#endregion
 
 // Rebuilding resolved and unresolved links (metadataCache.resolvedLinks and metadataCache.unresolvedLinks)
 // Adds the links to the outResolvedLinks and outUnresolvedLinks arrays.
-// function rebuildResolvedLinks(cachedMetadata: CachedMetadata, metadataCache: MetadataCache, outResolvedLinks: Record<string, number>, outUnresolvedLinks: Record<string, number>) {
-//     const links = cachedMetadata.links;
+function rebuildResolvedLinks(cachedMetadata: CachedMetadata, metadataCache: MetadataCache, path: string) {
+    const links = cachedMetadata.links;
+    const resolvedLinks = metadataCache.resolvedLinks[path];
+    const unresolvedLinks = metadataCache.unresolvedLinks[path];
 
-//     if (!links)
-//         return;
+    if (!links)
+        return;
 
-//     links.forEach((linkCache) => {
-//         const link = linkCache.link;
-//         const file = metadataCache.getFirstLinkpathDest(link, "");
-//         let path = "";
-//         let linksRef: Record<string, number>;
+    links.forEach((linkCache) => {
+        const link = linkCache.link;
+        const file = metadataCache.getFirstLinkpathDest(link, "");
+        let filePath = "";
+        let linksRef: Record<string, number>;
 
-//         // If can't find file, add to unresolvedLinks
-//         if (file === null) {
-//             linksRef = outUnresolvedLinks;
-//             path = link;
-//         }
-//         // If can find file, add to resolved links
-//         else {
-//             linksRef = outResolvedLinks;
-//             path = file.path;
-//         }
+        // If can't find file, add to unresolvedLinks
+        if (file === null) {
+            linksRef = unresolvedLinks;
+            filePath = link;
+        }
+        // If can find file, add to resolved links
+        else {
+            linksRef = resolvedLinks;
+            filePath = file.path;
+        }
 
-//         if (linksRef[path] === undefined) 
-//             console.warn("Reset")
-//         if (linksRef[path] === undefined) 
-//             linksRef[path] = 0;
+        if (linksRef[filePath] === undefined) 
+            linksRef[filePath] = 0;
         
-//         linksRef[path]++;
-//     })
-// }
+        linksRef[path]++;
+    })
+}
+
+function sortCacheByOffset(a: CacheItem, b: CacheItem): number {
+    const aStartOffset = a.position.start.offset;
+    const bStartOffset = b.position.start.offset;
+    if (aStartOffset < bStartOffset)
+        return -1;
+    else if (aStartOffset > bStartOffset)
+        return 1;
+    // Shouldn't happen
+    else {
+        console.warn("Heading with same offset");
+        return 0;
+    } 
+}
